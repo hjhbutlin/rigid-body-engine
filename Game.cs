fcd3903c0100a3548123ada54b496738e72cea4f
Henry Butlin <henrybutlin@Henrys-MacBook.local>
2025-01-30 23:31:53 +0000

    After multiple youtube tutorials, diving deep into stackoverflow, GL documentation, and chatgpt, rogram now renders a shape (triangle) with both vertex and fragment shader. Why didn't I just use sfml like the other project :'(

--- Game.cs
+++ Game.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK.Graphics.OpenGL4;
@@ -13,6 +14,18 @@ namespace physics_engine
 {
     internal class Game : GameWindow {
 
+
+        float[] vertices = 
+        {
+            0f, 0.5f, 0f,
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f
+        };
+
+
+        // Render Pipeline variables
+        int vao;
+        int shaderProgram;
         int width, height;
         public Game(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
         {
@@ -22,6 +35,109 @@ namespace physics_engine
             CenterWindow(new Vector2i(width,height));
 
         }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0,0,e.Width,e.Height);
+            this.width = e.Width;
+            this.height = e.Height;
+        }
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            vao = GL.GenVertexArray();
+
+            int vbo = GL.GenBuffer();
+            // bind vbo
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            // data into vbo
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length*sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            
+            // bind vao
+            GL.BindVertexArray(vao);
+            // slot 0, size 3, type, normalised?, stride, offset
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            // enable slot 0
+            GL.EnableVertexAttribArray(0);
+
+            // unbind both
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+
+            // create shader program
+            shaderProgram = GL.CreateProgram();
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader,LoadShaderSource("Default.vert"));
+            GL.CompileShader(vertexShader);
+
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader,LoadShaderSource("Default.frag"));
+            GL.CompileShader(fragmentShader);
+
+            GL.AttachShader(shaderProgram, vertexShader);
+            GL.AttachShader(shaderProgram, fragmentShader);
+
+            GL.LinkProgram(shaderProgram);
+
+            // delete all shaders
+
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            GL.DeleteVertexArray(vao);
+            GL.DeleteProgram(shaderProgram);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(0f,0f,0f,0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            // draw triangle
+
+            GL.UseProgram(shaderProgram);
+            GL.BindVertexArray(vao);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+
+            Context.SwapBuffers();
+
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        // function that loads a .txt and returns a str
+
+        public static string LoadShaderSource(String filePath)
+        {
+            string shaderSource = "";
+
+            try
+            {
+                using (StreamReader reader = new StreamReader("./Shaders/" + filePath))
+                {
+                    shaderSource = reader.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to read shader source file: " + e.Message);
+            }
+
+            return shaderSource;
+        }
+
     }
 
 
